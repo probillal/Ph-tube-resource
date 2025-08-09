a389c08c2472be619c987266f9b8bb004db2f49f
probillal <smbillalhossain273@gmail.com>
2025-08-09 21:39:14 +0600

    profile picture, profile and verified badge added

--- js/ph-tube.js
+++ js/ph-tube.js
@@ -33,18 +33,24 @@ const displayVideos = (videos) => {
     const card = document.createElement("div");
     card.classList = "card card-compact";
     card.innerHTML = `
-    <figure>
+    <figure class="h-[200px]">
     <img
       src="${video.thumbnail}"
-      alt="Shoes" />
+      alt="Shoes" class='w-full h-full object-cover'/>
   </figure>
-  <div class="card-body">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+  <div class="flex gap-2 px-0 py-2">
+    <div>
+        <img class="w-10 h-10 rounded-full object-cover" src="${video.authors[0].profile_picture}"/>
     </div>
-  </div>
+    <div>
+        <h2 class="font-bold">${video.title}</h2>
+         <div class="flex gap-2 items-center">
+        <div><p class="text-gray-400">${video.authors[0].profile_name}</p></div>
+        <div> <img class="w-5" src="https://img.icons8.com/?size=100&id=98A4yZTt9abw&format=png&color=000000"/></div>
+    </div>
+    </div>
+   
+  </.div>
     `;
     videoContainer.append(card);
   });
