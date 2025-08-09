7e134e36e3978769db4972b487e16198182a1b33
probillal <smbillalhossain273@gmail.com>
2025-08-09 22:08:39 +0600

    added render conditional data on dynamic card

--- js/ph-tube.js
+++ js/ph-tube.js
@@ -30,6 +30,7 @@ const displayCategories = (categories) => {
 const displayVideos = (videos) => {
   const videoContainer = document.getElementById("videos");
   videos.forEach((video) => {
+    console.log(video);
     const card = document.createElement("div");
     card.classList = "card card-compact";
     card.innerHTML = `
@@ -40,14 +41,24 @@ const displayVideos = (videos) => {
   </figure>
   <div class="flex gap-2 px-0 py-2">
     <div>
-        <img class="w-10 h-10 rounded-full object-cover" src="${video.authors[0].profile_picture}"/>
+        <img class="w-10 h-10 rounded-full object-cover" src="${
+          video.authors[0].profile_picture
+        }"/>
     </div>
     <div>
         <h2 class="font-bold">${video.title}</h2>
          <div class="flex gap-2 items-center">
         <div><p class="text-gray-400">${video.authors[0].profile_name}</p></div>
-        <div> <img class="w-5" src="https://img.icons8.com/?size=100&id=98A4yZTt9abw&format=png&color=000000"/></div>
+         ${
+           video.authors[0].verified === true
+             ? `<img
+               class="w-5"
+               src="https://img.icons8.com/?size=100&id=98A4yZTt9abw&format=png&color=000000"
+             />`
+             : " "
+         }
     </div>
+    <div><p>${video.others.views}</p></div>
     </div>
    
   </.div>
@@ -58,3 +69,22 @@ const displayVideos = (videos) => {
 
 loadCategories();
 loadVideos();
+
+// {
+//     "category_id": "1001",
+//     "video_id": "aaaa",
+//     "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+//     "title": "Shape of You",
+//     "authors": [
+//         {
+//             "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+//             "profile_name": "Olivia Mitchell",
+//             "verified": ""
+//         }
+//     ],
+//     "others": {
+//         "views": "100K",
+//         "posted_date": "16278"
+//     },
+//     "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+// }
