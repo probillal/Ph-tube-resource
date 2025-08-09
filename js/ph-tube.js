3755e9372d65186beaf96bd07a6c6ea7105d6f50
probillal <smbillalhossain273@gmail.com>
2025-08-09 11:50:11 +0600

    added card section in video container

--- js/ph-tube.js
+++ js/ph-tube.js
@@ -6,6 +6,13 @@ const loadCategories = () => {
     .then((data) => displayCategories(data.categories))
     .catch((error) => console.log(error()));
 };
+// create load videos
+const loadVideos = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.videos))
+    .catch((error) => console.log(error));
+};
 // create display categories
 const displayCategories = (categories) => {
   const categoriesContainer = document.getElementById("categories");
@@ -19,5 +26,29 @@ const displayCategories = (categories) => {
     categoriesContainer.append(button);
   });
 };
+// create display videos
+const displayVideos = (videos) => {
+  const videoContainer = document.getElementById("videos");
+  videos.forEach((video) => {
+    const card = document.createElement("div");
+    card.classList = "card card-compact";
+    card.innerHTML = `
+    <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+    `;
+    videoContainer.append(card);
+  });
+};
 
 loadCategories();
+loadVideos();
